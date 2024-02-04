7e6150ceb3e0a3556c8aa9acc8ec94dfd5310869
TanyaMayak <158187566+TanyaMayak@users.noreply.github.com>
2024-02-04 19:11:20 +0200

    Create Calc.dart
    Done

---
+++ Calc.dart
@@ -0,0 +1,68 @@
+import 'dart:io';
+
+void main(){
+  String input = '';
+  do{
+    input = getCommand(info: 'Введите строку вида \"8 * 10\": ');
+    if(input != 'exit')
+      doOp(input.trim());
+    else
+      stdout.write(input);
+  }while(input != 'exit');
+}
+
+void noticeErr() {
+  stdout.writeln('ERROR');
+}
+
+Function select(String char){
+  switch(char) {
+    case '+':
+      return (num a, num b) => a + b;
+    case '-':
+      return (num a, num b) => a - b;
+    case '*':
+      return (num a, num b) => a * b;
+    case '/':
+      return (num a, num b) => a / b;
+  }
+  return noticeErr;
+}
+
+bool checkOp(String comm){
+  const a = ['+', '-', '*', '/'];
+  var accept = comm.split(' ');
+  if(accept.length == 3) {
+    var firstN = num.tryParse(accept[0]);
+    if(firstN == null)
+      return false;
+    var sign = accept[1];
+    if(!a.contains(sign))
+      return false;
+    var secondN = num.tryParse(accept[2]);
+    if(secondN == null)
+      return false;
+  }
+  return true;
+}
+
+void doOp(String command){
+  if(checkOp(command) == false){
+    noticeErr();
+    return;
+  }
+  else{
+    var params = command.split(' ');
+    num firstN = num.parse(params[0]);
+    num secondN = num.parse(params[2]);
+
+    var op = select(params[1]);
+    stdout.write('$command = ' + op(firstN, secondN).toString() + '\n');
+  }
+}
+
+String getCommand({String info = ''}){
+  stdout.write(info);
+  String input = stdin.readLineSync()?.trim() ?? 'exit';
+  return input;
+}
